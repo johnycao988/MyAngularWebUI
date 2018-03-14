c3cb545363eb1b008102eb9cc412e4561f572e2b
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2018-03-14 15:55:43 +0800

    11

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -38,6 +38,7 @@ export class NavbarComponent implements OnInit{
 //                return this.listTitles[item].title;
 //            }
 //        }
+      
         return this.miService.getSelectedItemUrl();
     }
     sidebarToggle(){
